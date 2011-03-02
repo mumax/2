141280098d6837644b059f4d6162e478cd9db857
Arne Vansteenkiste <arne@monster2.(none)>
2011-03-02 01:48:13 +0100

    logger_test.go

--- src/pkg/common/logger.go
+++ src/pkg/common/logger.go
@@ -7,6 +7,10 @@
 
 package common
 
+// This file implements a global logger that prints to screen (stderr) and to a log file.
+// The screen output can be filtered/disabled, while all output always goes to the log file.
+// Author: Arne Vansteenkiste
+
 import(
 	"log"
 	"os"
@@ -16,30 +20,52 @@ import(
 var logger Logger
 
 type Logger struct{
-	ShowDebug bool
-	ShowWarn bool
-	ShowPrint bool
+	ShowDebug bool // Include debug messages in stderr output?
+	ShowWarn bool // Include warnings in stderr output?
+	ShowPrint bool// Include normal output in stderr output?
 	Screen *log.Logger // Logs to the screen (stderr), usually prints only limited output
 	File *log.Logger // Logs to a log file, usually prints all output (including debug)
 }
 
-func(l *Logger) Init(logfile string){
-	l.Screen = log.New(os.Stderr, "", log.Ltime | log.Lmicroseconds)
-	//out = 
-	//l.File = log.New(out, "", log.Ltime | log.Lmicroseconds)
+func InitLogger(logfile string){
+	logger.Init(logfile)
 }
 
+// Initiates the logger and sets a log file.
+func(l *Logger) Init(logfile string){
+	l.Screen = log.New(os.Stderr, "", 0)
+	out := FOpen(logfile)
+	l.File = log.New(out, "", log.Ltime | log.Lmicroseconds)
+	Debug("Opened log file:", logfile)
+}
+
+// Log a debug message.
 func Debug(msg ...interface{}){
 	if logger.ShowDebug{
 		logger.Screen.Println(msg...)
 	}
-	if logger.File != nil{logger.File.Println(msg...)}
+	LogFile(msg...)
 }
 
-func Warn(msg ...interface{}){
+const MSG_WARNING = "Warning:"
 
+// Log a warning.
+func Warning(msg ...interface{}){
+	if logger.ShowWarn{
+		logger.Screen.Println(msg...)
+	}
+	LogFile(msg...)
 }
 
+// Log normal output.
 func Println(msg ...interface{}){
+	if logger.ShowPrint{
+		logger.Screen.Println(msg...)
+	}
+	LogFile(msg...)
+}
 
+// Log to the log file only.
+func LogFile(msg ...interface{}){
+	if logger.File != nil{logger.File.Println(msg...)}
 }
