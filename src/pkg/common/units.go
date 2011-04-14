dde475a9457cbd19c4f9573a9f882d62cc7107e4
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-04-14 17:09:29 +0200

    units.go

---
+++ src/pkg/common/units.go
@@ -0,0 +1,22 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package common
+
+// This file implements internal units.
+// In internal units:
+// mu0 = 4 pi E-1
+// gamma0 = 2.211
+// unit field = 1MA/m
+// unit length = 1nm
+// Author: Arne Vansteenkiste.
+
+import ()
+
+type Units struct{
+
+}
