42d3d51e0aa7a2c4702d926caacfea03a0ec30f7
Arne <Arne.Vansteenkiste@UGent.be>
2011-06-09 15:58:31 +0200

    mv apigen->client

--- src/pkg/mumax/client/java.go
+++ src/pkg/mumax/client/java.go
@@ -5,7 +5,7 @@
 //  Note that you are welcome to modify this code under the condition that you do not remove any 
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
- 
+
 package apigen
 
 import (
@@ -26,7 +26,7 @@ func (j *Java) Comment() string {
 }
 
 func (j *Java) WriteHeader(out io.Writer) {
-	fmt.Fprintln(out,`
+	fmt.Fprintln(out, `
 public class Mumax2{
 	private void recv(){
 					
@@ -36,19 +36,23 @@ public class Mumax2{
 
 
 func (j *Java) WriteFooter(out io.Writer) {
-	fmt.Fprint(out,`}`)
+	fmt.Fprint(out, `}`)
 }
 
 
 func (j *Java) WriteFunc(out io.Writer, funcName string, argTypes []reflect.Type, returnType reflect.Type) {
 	fmt.Fprintln(out)
 	ret := "void"
-	if returnType != nil{ret = returnType.String()}
+	if returnType != nil {
+		ret = returnType.String()
+	}
 	fmt.Fprintf(out, `	public static %s %s(`, ret, funcName)
 
 	args := ""
 	for i := range argTypes {
-		if i != 0 { args += ", " }
+		if i != 0 {
+			args += ", "
+		}
 		args += returnType.String() + " "
 		args += "arg" + fmt.Sprint(i+1)
 	}
@@ -58,7 +62,7 @@ func (j *Java) WriteFunc(out io.Writer, funcName string, argTypes []reflect.Type
 	fmt.Fprintln(out)
 
 	for i := range argTypes {
-	fmt.Fprintln(out, "\t", "System.out.Print(\"\"+arg", i+1, ");")
+		fmt.Fprintln(out, "\t", "System.out.Print(\"\"+arg", i+1, ");")
 	}
 	fmt.Fprintln(out, "\t", "System.out.Println();")
 	fmt.Fprintln(out, "\t", "System.out.Flush();")
