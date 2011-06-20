3a747b496cead8898c1954cb6bbaa0ec3307b4b7
Arne <Arne.Vansteenkiste@UGent.be>
2011-06-20 10:25:14 +0200

    cleaned up engine/

--- src/pkg/mumax/engine/field.go
+++ src/pkg/mumax/engine/field.go
@@ -11,105 +11,105 @@ package engine
 // either scalar, vector or tensor fields in time and space.
 // Author: Arne Vansteenkiste.
 
-import (
-	. "mumax/common"
-	"fmt"
-)
-
-// Conceptually, each field is represented by A(r) * m(t), a pointwise multiplication
-// of an N-vector function of space A(r) by an N-vector function of time m(t).
-// A(r) is an array, m(t) is the multiplier.
+//import (
+//	. "mumax/common"
+//	"fmt"
+//)
 //
-// When the array is nil/NULL, the field is independent of space. The array is then
-// interpreted as 1(r), the unit field. In this way, quantities that are constant
-// over space (homogeneous) can be efficiently represented. These are also called values.
+//// Conceptually, each field is represented by A(r) * m(t), a pointwise multiplication
+//// of an N-vector function of space A(r) by an N-vector function of time m(t).
+//// A(r) is an array, m(t) is the multiplier.
+////
+//// When the array is nil/NULL, the field is independent of space. The array is then
+//// interpreted as 1(r), the unit field. In this way, quantities that are constant
+//// over space (homogeneous) can be efficiently represented. These are also called values.
+////
+//// When the array has only one component and the multiplier has N components,
+//// then the field has N components: a(r) * m0(t), a(r) * m1(t), ... a(r) * mN(t)
+////
+//type Field struct {
+//	array       Array // contains the size
+//	_multiplier [FIELD_MAX_COMP]float32
+//	multiplier  []float32
+//	name        string
+//}
 //
-// When the array has only one component and the multiplier has N components,
-// then the field has N components: a(r) * m0(t), a(r) * m1(t), ... a(r) * mN(t)
 //
-type Field struct {
-	array       Array // contains the size
-	_multiplier [FIELD_MAX_COMP]float32
-	multiplier  []float32
-	name        string
-}
-
-
-func newField(name string, nComp int, size3D []int) *Field {
-	f := new(Field)
-	f.Init(name, nComp, size3D)
-	return f
-}
-
-
-func newScalar(name string) *Field {
-	return newField(name, 1, nil)
-}
-
-func newVector(name string) *Field {
-	return newField(name, 3, nil)
-}
-
-func newScalarField(name string, size3D []int) *Field {
-	return newField(name, 1, size3D)
-}
-
-func newVectorField(name string, size3D []int) *Field {
-	return newField(name, 3, size3D)
-}
-
-// Initiates a field with nComp components and array size size3D.
-// When size3D == nil, the field is space-independent (homogeneous).
-func (f *Field) Init(name string, nComp int, size3D []int) {
-	Assert(nComp > 0 && nComp <= FIELD_MAX_COMP)
-	Assert(size3D == nil || len(size3D) == 3)
-
-	f.array.Free()
-	if size3D != nil {
-		f.array.Init(nComp, size3D)
-	}
-
-	for i := range f._multiplier {
-		f._multiplier[i] = 1
-	}
-	f.multiplier = f._multiplier[:nComp]
-
-	f.name = name
-}
-
-
-func (f *Field) Free() {
-	f.array.Free()
-	f.multiplier = nil
-	f.name = ""
-}
-
-func (f *Field) Name() string {
-	return f.name
-}
-
-// Number of components of the field values.
-// 1 = scalar, 3 = vector, etc.
-func (f *Field) NComp() int {
-	return len(f.multiplier)
-}
-
-func (f *Field) IsSpaceDependent() bool {
-	return !f.array.IsNil()
-}
-
-
-func (f *Field) String() string {
-	str := f.Name() + "(" + fmt.Sprint(f.NComp()) + "-vector "
-	if f.IsSpaceDependent() {
-		str += "field"
-	} else {
-		str += "value"
-	}
-	str += ")"
-	return str
-}
-
-// Maximum number of components of a Field.
-// 1 = scalar, 3 = vector, 6 = symmetric tensor, 9 = general tensor.
-const FIELD_MAX_COMP = 9
+//func newField(name string, nComp int, size3D []int) *Field {
+//	f := new(Field)
+//	f.Init(name, nComp, size3D)
+//	return f
+//}
+//
+//
+//func newScalar(name string) *Field {
+//	return newField(name, 1, nil)
+//}
+//
+//func newVector(name string) *Field {
+//	return newField(name, 3, nil)
+//}
+//
+//func newScalarField(name string, size3D []int) *Field {
+//	return newField(name, 1, size3D)
+//}
+//
+//func newVectorField(name string, size3D []int) *Field {
+//	return newField(name, 3, size3D)
+//}
+//
+//// Initiates a field with nComp components and array size size3D.
+//// When size3D == nil, the field is space-independent (homogeneous).
+//func (f *Field) Init(name string, nComp int, size3D []int) {
+//	Assert(nComp > 0 && nComp <= FIELD_MAX_COMP)
+//	Assert(size3D == nil || len(size3D) == 3)
+//
+//	f.array.Free()
+//	if size3D != nil {
+//		f.array.Init(nComp, size3D)
+//	}
+//
+//	for i := range f._multiplier {
+//		f._multiplier[i] = 1
+//	}
+//	f.multiplier = f._multiplier[:nComp]
+//
+//	f.name = name
+//}
+//
+//
+//func (f *Field) Free() {
+//	f.array.Free()
+//	f.multiplier = nil
+//	f.name = ""
+//}
+//
+//func (f *Field) Name() string {
+//	return f.name
+//}
+//
+//// Number of components of the field values.
+//// 1 = scalar, 3 = vector, etc.
+//func (f *Field) NComp() int {
+//	return len(f.multiplier)
+//}
+//
+//func (f *Field) IsSpaceDependent() bool {
+//	return !f.array.IsNil()
+//}
+//
+//
+//func (f *Field) String() string {
+//	str := f.Name() + "(" + fmt.Sprint(f.NComp()) + "-vector "
+//	if f.IsSpaceDependent() {
+//		str += "field"
+//	} else {
+//		str += "value"
+//	}
+//	str += ")"
+//	return str
+//}
+//
+//// Maximum number of components of a Field.
+//// 1 = scalar, 3 = vector, 6 = symmetric tensor, 9 = general tensor.
+//const FIELD_MAX_COMP = 9
