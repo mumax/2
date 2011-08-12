5e433ff766540144ea9767d487185b43f0b90473
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-12 10:25:08 +0200

    playing with indent -linux

--- src/lib/add.cu
+++ src/lib/add.cu
@@ -9,28 +9,24 @@
 extern "C" {
 #endif
 
-
 ///@internal
-__global__ void addKern(float* dst, float* a, float* b, int Npart){
-  int i = threadindex;
-  if(i < Npart){
-    dst[i] = a[i] + b[i];
-  }
+__global__ void addKern(float *dst, float *a, float *b, int Npart) {
+	int i = threadindex;
+	if (i < Npart) {
+		dst[i] = a[i] + b[i];
+	}
 }
 
-
-void add(float** dst, float** a, float** b, CUstream* stream, int Ndev, int Npart){
+void add(float **dst, float **a, float **b, CUstream * stream, int Ndev, int Npart) {
 	dim3 gridSize, blockSize;
 	make1dconf(Npart, &gridSize, &blockSize);
-	for(int i=0; i<Ndev; i++){
-		addKern<<<gridSize, blockSize, 0, cudaStream_t(stream[i])>>>(dst[i], a[i], b[i], Npart);
+	for (int i = 0; i < Ndev; i++) {
+		addKern <<< gridSize, blockSize, 0, cudaStream_t(stream[i]) >>> (dst[i], a[i], b[i], Npart);
 	}
-	for(int i=0; i<Ndev; i++){
+	for (int i = 0; i < Ndev; i++) {
 		cuStreamSynchronize(stream[i]);
 	}
 }
-
-
 
 #ifdef __cplusplus
 }
