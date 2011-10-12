b924c453a393b46e46d0db80d5a7ec95099efd63
Arne <Arne.Vansteenkiste@UGent.be>
2011-10-12 16:56:09 +0200

    found possible h-field bug (in mulitplier)

--- src/pkg/mumax/engine/mod_hfield.go
+++ src/pkg/mumax/engine/mod_hfield.go
@@ -31,6 +31,6 @@ func (x ModHField) Name() string {
 func (x ModHField) Load(e *Engine) {
 	e.AddQuant("H", VECTOR, FIELD, Unit("A/m"), "magnetic field")
 	q := e.Quant("H")
-	// multiplier ??
+	panic("need to set multiplier here? somewhere else?")
 	q.updater = &SumUpdater{q}
 }
