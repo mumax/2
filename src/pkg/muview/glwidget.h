c706be6b5fe51f202420e9d4ee93715df4cdc61f
Graham Rowlands <grahamrow@gmail.com>
2011-10-18 11:49:05 -0700

    libglut working with Qt, thankfully. Now drawing glutSolidCone happily in the viewer area. Next to implement is, inevitably, a host of cones! Will need to decide on the array formatting, for now I will assume we get a list of vector coordinates and a corresponding list of magnetization vector components.

--- src/pkg/muview/glwidget.h
+++ src/pkg/muview/glwidget.h
@@ -1,46 +1,47 @@
 #ifndef GLWIDGET_H
- #define GLWIDGET_H
+#define GLWIDGET_H
 
- #include <QGLWidget>
+#include <QGLWidget>
 
- class QtLogo;
+class QtLogo;
 
- class GLWidget : public QGLWidget
- {
-     Q_OBJECT
+class GLWidget : public QGLWidget
+{
+  Q_OBJECT
 
- public:
-     GLWidget(QWidget *parent = 0);
-     ~GLWidget();
+  public:
+  GLWidget(QWidget *parent = 0);
+  ~GLWidget();
 
-     QSize minimumSizeHint() const;
-     QSize sizeHint() const;
+  QSize minimumSizeHint() const;
+  QSize sizeHint() const;
 
- public slots:
-     void setXRotation(int angle);
-     void setYRotation(int angle);
-     void setZRotation(int angle);
+public slots:
+  void setXRotation(int angle);
+  void setYRotation(int angle);
+  void setZRotation(int angle);
 
- signals:
-     void xRotationChanged(int angle);
-     void yRotationChanged(int angle);
-     void zRotationChanged(int angle);
+signals:
+  void xRotationChanged(int angle);
+  void yRotationChanged(int angle);
+  void zRotationChanged(int angle);
 
- protected:
-     void initializeGL();
-     void paintGL();
-     void resizeGL(int width, int height);
-     void mousePressEvent(QMouseEvent *event);
-     void mouseMoveEvent(QMouseEvent *event);
+protected:
+  void initializeGL();
+  void paintGL();
+  void resizeGL(int width, int height);
+  void mousePressEvent(QMouseEvent *event);
+  void mouseMoveEvent(QMouseEvent *event);
 
- private:
-     QtLogo *logo;
-     int xRot;
-     int yRot;
-     int zRot;
-     QPoint lastPos;
-     QColor qtGreen;
-     QColor qtPurple;
- };
+private:
+  QtLogo *logo;
+  int xRot;
+  int yRot;
+  int zRot;
+  QPoint lastPos;
+  QColor qtGreen;
+  QColor qtPurple;
+  GLuint cone;
+};
 
- #endif
+#endif
