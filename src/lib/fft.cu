fb1063728a972d240722f3630072e2a811cd2a78
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-10-21 09:51:07 +0200

    add empty fft files

---
+++ src/lib/fft.cu
@@ -0,0 +1,15 @@
+#include "fft.h"
+
+#include "multigpu.h"
+#include <cuda.h>
+#include "gpu_conf.h"
+#include "gpu_safe.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/lib/fft.h
@@ -0,0 +1,21 @@
+/**
+  * @file
+  *
+  * @author Arne Vansteenkiste
+  */
+
+#ifndef _FFT_H_
+#define _FFT_H_
+
+#include <cuda.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
