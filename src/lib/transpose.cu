20346d5a47ce830f270354b908f27920830517a6
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-03 15:44:57 +0100

    transpose fix

--- src/lib/transpose.cu
+++ src/lib/transpose.cu
@@ -27,34 +27,34 @@ __global__ void xdevTransposePadKernel(complex* output, complex* input, int N1, 
 
 	for(int x=0; x<N0; x++){ // could take this out of kernel if we stream over planes...
 
-    // index of the block inside the blockmatrix
-    int BI = blockIdx.x;
-    int BJ = blockIdx.y;
+   		 // index of the block inside the blockmatrix
+   		 int BI = blockIdx.x;
+   		 int BJ = blockIdx.y;
 
-    // "minor" indices inside the tile
-    int i = threadIdx.x;
-    int j = threadIdx.y;
-    
-    // "major" indices inside the entire matrix
-    int I = BI * BLOCKSIZE + i;
-    int J = BJ * BLOCKSIZE + j;
+   		 // "minor" indices inside the tile
+   		 int i = threadIdx.x;
+   		 int j = threadIdx.y;
+   		 
+   		 // "major" indices inside the entire matrix
+   		 int I = BI * BLOCKSIZE + i;
+   		 int J = BJ * BLOCKSIZE + j;
 
-    if((I < N1) && (J < chunkN2)){ // must be within this device's chunk (chunkN2)
-      block[j][i] = input[x*N1*N2 + J * N1 + I]; // but indexes in total data (N2)
-    }
-    
-    __syncthreads();
+   		 if((I < N1) && (J < chunkN2)){ // must be within this device's chunk (chunkN2)
+   		   block[j][i] = input[x*N1*N2 + J * N1 + I]; // but indexes in total data (N2)
+   		 }
+   		 
+   		 __syncthreads();
 
-    
-    // Major indices with transposed blocks but not transposed minor indices
-    int It = BJ * BLOCKSIZE + i;
-    int Jt = BI * BLOCKSIZE + j;
+   		 
+   		 // Major indices with transposed blocks but not transposed minor indices
+   		 int It = BJ * BLOCKSIZE + i;
+   		 int Jt = BI * BLOCKSIZE + j;
 
-    if((It < chunkN2) && (Jt < N1)){
-      output[x*N1*N2 + Jt * N2 + It] = block[i][j];
-    }
+   		 if((It < chunkN2) && (Jt < N1)){
+   		   output[x*N1*N2 + Jt * N2 + It] = block[i][j];
+   		 }
     
-    __syncthreads();
+   		 __syncthreads();
 
 	}
 }
@@ -62,6 +62,7 @@ __global__ void xdevTransposePadKernel(complex* output, complex* input, int N1, 
 
 void transposePadYZAsync(float** output_f, float** input_f, int N0, int N1Part, int N2, int N2Pad, CUstream* stream){
 	int nDev = nDevice();
+
 	N2 /= 2; // number of complexes
 	complex** input = (complex**)input_f;
 	complex** output = (complex**)output_f;
@@ -82,7 +83,7 @@ void transposePadYZAsync(float** output_f, float** input_f, int N0, int N1Part, 
 			// source device = dev
 			// target device = chunk
 			complex* src = &(input[dev][chunk*chunkN2]); // offset device pointer to start of chunk
-			complex* dst = &(output[chunk][chunk*chunkN2]); // offset device pointer to start of chunk
+			complex* dst = &(output[chunk][dev*chunkN2]); // offset device pointer to start of chunk
 
     		xdevTransposePadKernel<<<gridsize, blocksize, 0, stream[dev]>>>(dst, src, N2, N1Part, N0, chunkN2); // yes N1-N2 are reversed.
 		
