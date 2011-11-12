82e717f5a2824558fb5b60312ec5e6ff3897e200
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-12 21:54:16 +0100

    mv ovf2 code to format_ovf2.go

--- src/pkg/mumax/engine/format_ovf2.go
+++ src/pkg/mumax/engine/format_ovf2.go
@@ -22,17 +22,17 @@ import (
 )
 
 func init() {
-	RegisterOutputFormat(&FormatOmf{})
+	RegisterOutputFormat(&FormatOvf2{})
 }
 
-// OMF 1.0 output format
-type FormatOmf struct{}
+// OVF 2.0 output format
+type FormatOvf2 struct{}
 
-func (f *FormatOmf) Name() string {
-	return "omf"
+func (f *FormatOvf2) Name() string {
+	return "ovf"
 }
 
-func (f *FormatOmf) Write(out io.Writer, q *Quant, options []string) {
+func (f *FormatOvf2) Write(out io.Writer, q *Quant, options []string) {
 	dataformat := ""
 	switch len(options) {
 	case 0: // No options: default=Binary 4
@@ -40,73 +40,84 @@ func (f *FormatOmf) Write(out io.Writer, q *Quant, options []string) {
 	case 1:
 		dataformat = options[0]
 	default:
-		panic(InputErr(fmt.Sprint("Illegal OMF options:", options)))
+		panic(InputErr(fmt.Sprint("Illegal OVF options:", options)))
 	}
 
-	writeOmfHeader(out, q)
-	writeOmfData(out, q, dataformat)
+	writeOvf2Header(out, q)
+	writeOvf2Data(out, q, dataformat)
 	hdr(out, "End", "Segment")
 }
 
 
-const (
-	OMF_CONTROL_NUMBER = 1234567.0 // The omf format requires the first encoded number in the binary data section to be this control number
-)
-
-func writeOmfData(out io.Writer, q *Quant, dataformat string) {
+func writeOvf2Data(out io.Writer, q *Quant, dataformat string) {
 
 	hdr(out, "Begin", "Data "+dataformat)
 	switch strings.ToLower(dataformat) {
 	case "text":
 		q.Buffer().WriteAscii(out)
 	case "binary 4":
-		writeOmfBinary4(out, q.Buffer())
+		writeOvf2Binary4(out, q.Buffer())
 	default:
-		panic(InputErr("Illegal OMF data format " + dataformat + ". Options are: Text, Binary 4"))
+		panic(InputErr("Illegal OVF data format " + dataformat + ". Options are: Text, Binary 4"))
 	}
 	hdr(out, "End", "Data "+dataformat)
 }
 
-
-// Writes the OMF header
-func writeOmfHeader(out io.Writer, q *Quant) {
+func writeOvf2Header(out io.Writer, q *Quant) {
 	gridsize := GetEngine().GridSize()
 	cellsize := engine.CellSize()
 
-	hdr(out, "OOMMF", "rectangular mesh v1.0")
+	hdr(out, "OOMMF", "OVF 2.0")
+	fmt.Fprintln(out, "#")
 	hdr(out, "Segment count", "1")
+	fmt.Fprintln(out, "#")
 	hdr(out, "Begin", "Segment")
-
 	hdr(out, "Begin", "Header")
+	fmt.Fprintln(out, "#")
 
-	dsc(out, "Time", GetEngine().time.Scalar())
-	hdr(out, "Title", q.Name())
+	hdr(out, "title", "mumax data") // TODO
 	hdr(out, "meshtype", "rectangular")
 	hdr(out, "meshunit", "m")
-	hdr(out, "xbase", 0)
-	hdr(out, "ybase", 0)
-	hdr(out, "zbase", 0)
-	hdr(out, "xstepsize", cellsize[Z])
-	hdr(out, "ystepsize", cellsize[Y])
-	hdr(out, "zstepsize", cellsize[X])
+
 	hdr(out, "xmin", 0)
 	hdr(out, "ymin", 0)
 	hdr(out, "zmin", 0)
+
 	hdr(out, "xmax", cellsize[Z]*float64(gridsize[Z]))
 	hdr(out, "ymax", cellsize[Y]*float64(gridsize[Y]))
 	hdr(out, "zmax", cellsize[X]*float64(gridsize[X]))
+
+	// TODO: quant
+	hdr(out, "valuedim", 3)
+	hdr(out, "valuelabels", "Val_x", "Val_y", "Val_z") // TODO
+	unit := q.Unit()
+	if unit == "" {
+		unit = "1"
+	}
+	hdr(out, "valueunits", unit, unit, unit)
+
+	totaltime := GetEngine().time.Scalar()
+	// We don't really have stages
+	fmt.Fprintln(out, "# desc: Stage simulation time: ", totaltime, " s")
+	fmt.Fprintln(out, "# desc: Total simulation time: ", totaltime, " s")
+
+	hdr(out, "xbase", cellsize[Z]/2)
+	hdr(out, "ybase", cellsize[Y]/2)
+	hdr(out, "zbase", cellsize[X]/2)
+
 	hdr(out, "xnodes", gridsize[Z])
 	hdr(out, "ynodes", gridsize[Y])
 	hdr(out, "znodes", gridsize[X])
-	hdr(out, "ValueRangeMinMag", 1e-08) // not so "optional" as the OOMMF manual suggests...
-	hdr(out, "ValueRangeMaxMag", 1)     // TODO
-	hdr(out, "valueunit", q.Unit())
-	hdr(out, "valuemultiplier", 1)
 
+	hdr(out, "xstepsize", cellsize[Z])
+	hdr(out, "ystepsize", cellsize[Y])
+	hdr(out, "zstepsize", cellsize[X])
+	fmt.Fprintln(out, "#")
 	hdr(out, "End", "Header")
+	fmt.Fprintln(out, "#")
 }
 
-func writeOmfBinary4(out io.Writer, array *host.Array) {
+func writeOvf2Binary4(out io.Writer, array *host.Array) {
 	data := array.Array
 	gridsize := array.Size3D
 
@@ -127,9 +138,9 @@ func writeOmfBinary4(out io.Writer, array *host.Array) {
 		for j := 0; j < gridsize[Y]; j++ {
 			for k := 0; k < gridsize[Z]; k++ {
 				for c := Z; c >= X; c-- {
-					// dirty conversion from float32 to [4]byte
 					bytes = (*[4]byte)(unsafe.Pointer(&data[c][i][j][k]))[:]
-					bytes[0], bytes[1], bytes[2], bytes[3] = bytes[3], bytes[2], bytes[1], bytes[0]
+					// dirty conversion from float32 to [4]byte
+					//bytes[0], bytes[1], bytes[2], bytes[3] = bytes[3], bytes[2], bytes[1], bytes[0]
 					out.Write(bytes)
 				}
 			}
@@ -137,17 +148,3 @@ func writeOmfBinary4(out io.Writer, array *host.Array) {
 	}
 }
 
-func floats2bytes(floats []float32) []byte {
-	return (*[4]byte)(unsafe.Pointer(&floats[0]))[:]
-}
-
-// Writes a header key/value pair to out:
-// # Key: Value
-func hdr(out io.Writer, key string, value ...interface{}) {
-	fmt.Fprint(out, "# ", key, ": ")
-	fmt.Fprintln(out, value...)
-}
-
-func dsc(out io.Writer, k, v interface{}) {
-	hdr(out, "Desc", k, ": ", v)
-}
