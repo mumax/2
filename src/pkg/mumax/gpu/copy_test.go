1e06ea430ecba39f60310656b37bba71900fa10d
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-16 12:07:16 +0100

    skip copyDtoD bench on one GPU

--- src/pkg/mumax/gpu/copy_test.go
+++ src/pkg/mumax/gpu/copy_test.go
@@ -12,21 +12,26 @@ package gpu
 // Author: Arne Vansteenkiste
 
 import (
-. "mumax/common"
-cu "cuda/driver"
-"testing"
-"fmt"
+	. "mumax/common"
+	cu "cuda/driver"
+	"testing"
+	"fmt"
 )
 
-func BenchmarkCopyDtoD(bench *testing.B) {
+func BenchmarkCopyDtoD1M(bench *testing.B) {
+	benchmarkCopyDtoD(bench, 1024)
+}
 
+
+func benchmarkCopyDtoD(bench *testing.B, N int){
 	if NDevice() < 2 {
 		fmt.Println("Skipping BenchmarkCopyDtoD")
+		return
 	}
 
 	bench.StopTimer()
-	N1 := 1024
-	N2 := 1024 
+	N1 := N
+	N2 := N
 	size := []int{1, N1, N2}
 
 	a := NewArray(1, size)
@@ -35,11 +40,11 @@ func BenchmarkCopyDtoD(bench *testing.B) {
 	ptr1 := a.pointer[0]
 	ptr2 := a.pointer[1]
 
-	bytes:=SIZEOF_FLOAT*int64(a.PartLen3D())
+	bytes := SIZEOF_FLOAT * int64(a.PartLen3D())
 	cu.MemcpyDtoD(ptr2, ptr1, bytes)
 	bench.SetBytes(bytes)
 	bench.StartTimer()
-	for i:=0; i<bench.N; i++{
+	for i := 0; i < bench.N; i++ {
 		cu.MemcpyDtoD(ptr2, ptr1, SIZEOF_FLOAT*int64(a.PartLen3D()))
 	}
 }
