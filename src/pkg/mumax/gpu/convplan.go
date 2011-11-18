8219b177700b9731bdc8f01271cf3615f6377bea
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-18 14:31:48 +0100

    conv_test

--- src/pkg/mumax/gpu/convplan.go
+++ src/pkg/mumax/gpu/convplan.go
@@ -12,34 +12,65 @@ package gpu
 
 import (
 	. "mumax/common"
+	"mumax/host"
 	//	cu "cuda/driver"
 	//	"cuda/cufft"
 	//	"fmt"
 )
 
 type ConvPlan struct {
-	dataSize [3]int    // Size of the (non-zero) input data block
-	kernSize [3]int    // Kernel size >= dataSize
-	kernel   [9]*Array //kernel components 
+	dataSize [3]int   // Size of the (non-zero) input data block
+	kernSize [3]int   // Kernel size >= dataSize
+	fftKern  [6]Array // transformed kernel components, unused ones are nil.
 }
 
-func (conv *ConvPlan) Init(dataSize, kernSize []int) {
+// indices for (anti-)symmetric kernel when only 6 of the 9 components are stored.
+const (
+	XX = 0
+	YY = 1
+	ZZ = 2
+	YZ = 3
+	XZ = 4
+	XY = 5
+)
+
+var kernCompString map[int]string = map[int]string{XX: "XX", YY: "YY", ZZ: "ZZ", YZ: "YZ", XZ: "XZ", XY: "XY"}
+
+func (conv *ConvPlan) Init(dataSize []int, kernel []*host.Array) {
 	Assert(len(dataSize) == 3)
-	Assert(len(kernSize) == 3)
-	//NDev := NDevice()
+	Assert(len(kernel) == 6)
+
+	// find non-nil kernel element to get kernel size
+	var kernSize []int
+	for _, k := range kernel {
+		if k != nil {
+			kernSize = k.Size3D
+			break
+		}
+	}
 
 	// init size
 	for i := range conv.dataSize {
 		conv.dataSize[i] = dataSize[i]
 		conv.kernSize[i] = kernSize[i]
 	}
+
+	Debug("ConvPlan.init", "dataSize:", dataSize, "kernSize:", kernSize)
+
+	// init fftKern
+	for i, k := range kernel {
+		if k != nil {
+			Debug("ConvPlan.init", "alloc:", kernCompString[i])
+			conv.fftKern[i].Init(1, kernSize, DO_ALLOC)
+		}
+	}
 }
 
-func NewConvPlan(dataSize, kernSize []int) *ConvPlan {
-	conv := new(ConvPlan)
-	conv.Init(dataSize, kernSize)
-	return conv
-}
+//func NewConvPlan(dataSize,  []int) *ConvPlan {
+//	conv := new(ConvPlan)
+//	conv.Init(dataSize, kernSize)
+//	return conv
+//}
 
 func (conv *ConvPlan) Free() {
 	// TODO
