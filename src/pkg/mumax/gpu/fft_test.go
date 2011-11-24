182171263c03bece7c3e7bf615808a7f988aeb3d
ben <ben.vandewiele@ugent.be>
2011-11-24 15:58:04 +0100

    test added for symmetrical input FFT

--- src/pkg/mumax/gpu/fft_test.go
+++ src/pkg/mumax/gpu/fft_test.go
@@ -23,9 +23,9 @@ func TestFFT(test *testing.T) {
 	fmt.Println("")
 	fmt.Println("FFT Test")
 	nComp := 1
-	N0, N1, N2 := 4, 8, 8
+	N0, N1, N2 := 8, 8, 8
 	dataSize := []int{N0, N1, N2}
-	fftSize := []int{2 * N0, 2 * N1, 2 * N2}
+	fftSize := []int{N0, N1, N2}
 
 	if N0 == 1 { //2D case, no padding in x-direction
 		fftSize[0] = N0
@@ -38,26 +38,76 @@ func TestFFT(test *testing.T) {
 	out := NewArray(nComp, []int{fftSize[0], fftSize[1], fftSize[2] + 2*NDevice()})
 	inh := in.LocalCopy()
 
-	a := inh.Array[0]
-	n := 0
-	for i := 0; i < N0; i++ {
-		// 		n  = 0
-		for j := 0; j < N1; j++ {
-			for k := 0; k < N2; k++ {
-				// 				if i == 0 {
-				a[i][j][k] = float32(1)
-				// 				}
-				n++
+
+
+//   a := inh.Array[0]
+//   n := 0
+//   for i := 0; i < N0; i++ {
+//     //    n  = 0
+//     for j := 0; j < N1; j++ {
+//       for k := 0; k < N2; k++ {
+//         //        if i == 0 {
+//         a[i][j][k] = float32(1)
+//         //        }
+//         n++
+//       }
+//     }
+//   }
+//   inh.List[0] = 1
+
+// test for symmetrical input
+  a := inh.Array[0]
+
+  for i := 0; i < N0; i++ {
+    for j := 0; j < N1; j++ {
+      for k := 0; k < N2; k++ {
+        a[i][j][k] = 0
+      }
+    }
+  }
+
+  maxN0 :=1
+  if N0>1{
+    maxN0=N0/2
+  }
+ 
+  
+	for i := 0; i < maxN0; i++ {
+		for j := 0; j < N1/2; j++ {
+			for k := 0; k < N2/2; k++ {
+				a[i][j][k] = float32(i+j+k)
+				if (i>0){
+          a[N0-i][j][k] = float32(i+j+k)
+        }
+        if (j>0){
+          a[i][N1-j][k] = float32(i+j+k)
+        }
+        if (k>0){
+          a[i][j][N2-k] = float32(i+j+k)
+        }
+        if (i>0 && j>0){
+          a[N0-i][N1-j][k] = float32(i+j+k)
+        }
+        if (j>0 && k>0){
+          a[i][N1-j][N2-k] = float32(i+j+k)
+        }
+        if (i>0 && k>0){
+          a[N0-i][j][N2-k] = float32(i+j+k)
+        }
+        if (i>0 && j>0 && k>0){
+          a[N0-i][N1-j][N2-k] = float32(i+j+k)
+        }
 			}
 		}
 	}
-	inh.List[0] = 1
+	
+	fmt.Println (a)
 
 	//inh.List[0] = 1
 	in.CopyFromHost(inh)
 
 	fft.Forward(in, out)
-	fft.Inverse(out, in)
+// 	fft.Inverse(out, in)
 
 }
 
