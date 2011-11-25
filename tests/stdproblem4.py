dad0594d1dc3428e288300fc7caecd6d016a7961
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-25 11:07:08 +0100

    stdproblem4.py: relax first

--- tests/stdproblem4.py
+++ tests/stdproblem4.py
@@ -3,7 +3,7 @@ from mumax2 import *
 # Standard Problem 4
 
 
-setgridsize(128, 128, 1)
+setgridsize(128, 32, 1)
 setcellsize(500e-9, 125e-9, 3e-9)
 
 load('micromagnetism')
@@ -12,19 +12,23 @@ load('demagexch')
 setscalar('Msat', 800e3)
 setscalar('Aex', 1.3e-11)
 setscalar('alpha', 1)
-
+setscalar('dt', 1e-12)
 m=[ [[[1]]], [[[1]]], [[[0]]] ]
 setarray('m', m)
+
+
+autosave("m", "omf", ["Text"], 10e-12)
+autotabulate(["t", "<m>"], "m.txt", 10e-12)
+
+steps(1000)
 
 Hx = -24.6E-3 / mu0
 Hy =   4.3E-3 / mu0
 Hz =   0      / mu0 
-
 setvalue('H_ext', [Hx, Hy, Hz])
 
-setscalar('dt', 1e-12)
-autosave("m", "omf", ["Text"], 10e-12)
-autotabulate(["t", "<m>"], "m.txt", 10e-12)
+setscalar('alpha', 0.02)
+setscalar('dt', 0.1e-12)
 
 steps(10000)
 
