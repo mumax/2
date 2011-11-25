57092f527d601bf08018c3ecc5f70a34abb86bc7
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-11-25 11:25:01 +0100

    Who can make standard problem 4 work?

--- tests/stdproblem4.py
+++ tests/stdproblem4.py
@@ -12,15 +12,16 @@ load('demagexch')
 setscalar('Msat', 800e3)
 setscalar('Aex', 1.3e-11)
 setscalar('alpha', 1)
-setscalar('dt', 1e-12)
+setscalar('dt', 10e-12)
 m=[ [[[1]]], [[[1]]], [[[0]]] ]
 setarray('m', m)
 
 
-autosave("m", "omf", ["Text"], 10e-12)
+autosave("m", "omf", ["Text"], 100e-12)
 autotabulate(["t", "<m>"], "m.txt", 10e-12)
+autotabulate(["t", "<H>"], "H.txt", 10e-12)
 
-steps(1000)
+steps(10000)
 
 Hx = -24.6E-3 / mu0
 Hy =   4.3E-3 / mu0
@@ -30,7 +31,7 @@ setvalue('H_ext', [Hx, Hy, Hz])
 setscalar('alpha', 0.02)
 setscalar('dt', 0.1e-12)
 
-steps(10000)
+#steps(10000)
 
 printstats()
 savegraph("graph.png")
