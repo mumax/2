f1e032eb4eaf55cb3fd8a9818967467ff2f18d26
Rémy Lassalle-Balier <lassallebalier.remy@gmail.com>
2011-11-25 09:57:54 +0000

    some incomplete fixes on uniform initialization of quants

--- src/libmumax2/uniform.cu
+++ src/libmumax2/uniform.cu
@@ -27,14 +27,14 @@ __global__ void initScalarQuantUniformRegionKern(float* S, float* regions, float
 void initScalarQuantUniformRegionAsync(float** S, float** regions, float* host_initValues, int initValNum, CUstream* stream, int Npart) {
 	dim3 gridSize, blockSize;
 	make1dconf(Npart, &gridSize, &blockSize);
+	float* dev_initValues;
 	for (int dev = 0; dev < nDevice(); dev++) {
 		assert(S[dev] != NULL);
 		assert(regions[dev] != NULL);
 		assert(host_initValues != NULL);
 		gpu_safe(cudaSetDevice(deviceId(dev)));
-		float* dev_initValues;
-		cudaMalloc( (void**)&dev_initValues,initValNum * sizeof(float));
-		cudaMemcpy(dev_initValues,host_initValues,initValNum * sizeof(float), cudaMemcpyHostToDevice);
+		gpu_safe( cudaMalloc( (void**)&dev_initValues,initValNum * sizeof(float)));
+		gpu_safe( cudaMemcpy(dev_initValues,host_initValues,initValNum * sizeof(float), cudaMemcpyHostToDevice));
 		initScalarQuantUniformRegionKern <<<gridSize, blockSize, 0, cudaStream_t(stream[dev])>>> (S[dev],regions[dev],dev_initValues, initValNum, Npart);
 		cudaFree(dev_initValues);
 	}
@@ -58,20 +58,28 @@ __global__ void initVectorQuantUniformRegionKern(float* Sx, float* Sy, float* Sz
 }
 
 
-void initVectorQuantUniformRegionAsync(float** Sx, float** Sy, float** Sz, float** regions, float* initValuesX, float* initValuesY, float* initValuesZ, CUstream* stream, int Npart) {
+void initVectorQuantUniformRegionAsync(float** Sx, float** Sy, float** Sz, float** regions, float* host_initValuesX, float* host_initValuesY, float* host_initValuesZ, int initValNum, CUstream* stream, int Npart) {
 	dim3 gridSize, blockSize;
 	make1dconf(Npart, &gridSize, &blockSize);
-	int initValNum = sizeof(initValuesX) / sizeof(float);
+	float* dev_initValuesX;
+	float* dev_initValuesY;
+	float* dev_initValuesZ;
 	for (int dev = 0; dev < nDevice(); dev++) {
 		assert(Sx[dev] != NULL);
 		assert(Sy[dev] != NULL);
 		assert(Sz[dev] != NULL);
 		assert(regions[dev] != NULL);
-		assert(initValuesX != NULL);
-		assert(initValuesY != NULL);
-		assert(initValuesZ != NULL);
+		assert(host_initValuesX != NULL);
+		assert(host_initValuesY != NULL);
+		assert(host_initValuesZ != NULL);
 		gpu_safe(cudaSetDevice(deviceId(dev)));
-		initVectorQuantUniformRegionKern <<<gridSize, blockSize, 0, cudaStream_t(stream[dev])>>> (Sx[dev],Sy[dev],Sz[dev],regions[dev],initValuesX,initValuesY,initValuesZ, initValNum, Npart);
+		gpu_safe( cudaMalloc( (void**)&dev_initValuesX,initValNum * sizeof(float)));
+		gpu_safe( cudaMalloc( (void**)&dev_initValuesY,initValNum * sizeof(float)));
+		gpu_safe( cudaMalloc( (void**)&dev_initValuesZ,initValNum * sizeof(float)));
+		gpu_safe( cudaMemcpy(dev_initValuesX,host_initValuesX,initValNum * sizeof(float), cudaMemcpyHostToDevice));
+		gpu_safe( cudaMemcpy(dev_initValuesX,host_initValuesY,initValNum * sizeof(float), cudaMemcpyHostToDevice));
+		gpu_safe( cudaMemcpy(dev_initValuesX,host_initValuesZ,initValNum * sizeof(float), cudaMemcpyHostToDevice));
+		initVectorQuantUniformRegionKern <<<gridSize, blockSize, 0, cudaStream_t(stream[dev])>>> (Sx[dev],Sy[dev],Sz[dev],regions[dev],dev_initValuesX,dev_initValuesY,dev_initValuesZ, initValNum, Npart);
 	}
 }
 
