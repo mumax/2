60b011ebb19bff8ba22ae25699c6d22735edcc0f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-02 16:09:30 +0100

    auto-generate libmumax2.h from other headers

---
+++ src/test.module/test.h
@@ -0,0 +1,24 @@
+/**
+  * @file
+  * Functions for debugging only.
+  *
+  * @author Arne Vansteenkiste
+  */
+
+#ifndef _TEST_TEST_H_
+#define _TEST_TEST_H_
+
+#include <cuda.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void test_func(float** dst, int NPart);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
