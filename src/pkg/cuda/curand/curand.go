cdf0d824575440b420e4afe0d4ec9dfd127d8a86
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-05 10:12:01 +0100

    start cuda/curand

---
+++ src/pkg/cuda/curand/curand.go
@@ -0,0 +1,13 @@
+// Copyright 2011 Arne Vansteenkiste (barnex@gmail.com).  All rights reserved.
+// Use of this source code is governed by a freeBSD
+// license that can be found in the LICENSE.txt file.
+
+package curand
+
+//#include <curand.h>
+import "C"
+
+import (
+	//"unsafe"
+)
+
