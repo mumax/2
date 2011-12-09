2e65e284e48f6aef7259879fc2609b9f477173e2
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-09 13:20:23 +0100

    pointwise updater

--- src/pkg/mumax/engine/update_pointwise.go
+++ src/pkg/mumax/engine/update_pointwise.go
@@ -7,12 +7,58 @@
 
 package engine
 
-import ()
+import (
+	. "mumax/common"
+)
 
 type PointwiseUpdater struct {
-
+	quant *Quant
+	lastIdx int          // Index of last time, for fast lookup of next
+	points  [][]float64 // List of time+value lines: [time0, valx, valy, valz], [time1, ...
 }
 
-func (n *PointwiseUpdater) Update() {
-	panic("unimpl.")
+
+func (field *PointwiseUpdater) Update() {
+	if len(field.points) < 2 {
+		panic(InputErr("Pointwise definition needs at least two points"))
+	}
+	time := engine.time.Scalar()
+
+	//find closest times
+
+	// first search backwards in time, 
+	// multi-stage solvers may have gone back in time.
+	i:=0
+	for i= field.lastIdx; i >= 0; i-- {
+		if field.points[i][0] < time {
+			break
+		}
+	}
+	// then search forward
+	for ; i < len(field.points); i++ {
+		if field.points[i][0] >= time {
+			break
+		}
+	}
+	// i now points to a time >= engine.time
+	field.lastIdx = i
+
+	// out of range: value = unchanged
+	if i-1 < 0 || i >= len(field.points) {
+		// or should we zero it?
+		return 
+	}
+
+
+	t1 := field.points[i-1][0]
+	t2 := field.points[i][0]
+	v1 := field.points[i-1][1:]
+	v2 := field.points[i][1:]
+	dt := t2 - t1 //pt2[0] - pt1[0]
+	t := (time - t1) / dt // 0..1
+	value := field.quant.multiplier
+	for i := range value{
+		value[i] = v1[i] + t*(v2[i]-v1[i])
+	}
 }
+
