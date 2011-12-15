dbc8801b1a13d49baa1d9dd30adfa55165b28c56
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-15 09:47:13 +0100

    pack.sh compiles and creates a tarball.

---
+++ pack.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Produces a tarball with mumax binaries
+
+tarball=mumax2.tar.gz
+rm -f $tarball
+rm -rf examples/*.out tests/*.out
+files=$(ls)
+
+echo packing into $tarball: $files
+
+rm -rf $tarball.tar.gz
+clean_output="rm -rf examples/*.out tests/*.out src/*.mod/tests/*.out"
+
+make clean && make -j 4 && make test && make doc && $clean_output && tar cv $files | gzip > $tarball
+
+
