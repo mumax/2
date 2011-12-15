904b77941632fc428281bbd9fcfeb160e6d40cad
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-15 10:06:11 +0100

    pack.sh fixes

--- pack.sh
+++ pack.sh
@@ -2,16 +2,21 @@
 
 # Produces a tarball with mumax binaries
 
-tarball=mumax2.tar.gz
+dir=mumax2
+tarball=$dir.tar.gz
+rm -rf $dir
+mkdir $dir
 rm -f $tarball
 rm -rf examples/*.out tests/*.out
+rm -f test.log
 files=$(ls | grep -v pack.sh)
+cp -rv $files $dir
 
 echo packing into $tarball: $files
 
 rm -rf $tarball.tar.gz
 clean_output="rm -rf examples/*.out tests/*.out src/*.mod/tests/*.out"
 
-make clean && make -j 4 && make test && make doc && $clean_output && tar cv $files | gzip > $tarball
+make clean && make -j 4 && make test && make doc && $clean_output && tar cv $dir | gzip > $tarball
 
 
