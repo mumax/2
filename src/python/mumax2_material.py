1cfbf2275529e8d06ee80f054cc908c925260a8d
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-12-26 20:55:56 +0100

    add mumax2_material with standard material parameters

---
+++ src/python/mumax2_material.py
@@ -0,0 +1,27 @@
+## @package mumax2_material
+# Material parameters of common materials
+# @author Arne Vansteenkiste
+
+## Material parameters for standard Permalloy
+class Py:
+	msat=860e3
+	aexch=13e-12
+
+## Material parameters for standard Iron
+class Fe:
+	msat=1700e3
+	aexch=21e-12
+	ku1=48e3
+
+## Material parameters for standard Nickel
+class Ni:
+	msat=490e3
+	aexch=9e-12
+	ku1=-5.7e3
+
+## Material parameters for standard HCP Cobalt
+class Co:
+	msat=1400e3
+	aexch=30e-12
+	ku1=520e3
+
