5bbbe34cbdbfebb71c15d11607b63527098e80f5
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-01-11 15:52:00 +0100

    release 1603

--- src/pkg/mumax/frontend/main.go
+++ src/pkg/mumax/frontend/main.go
@@ -266,7 +266,7 @@ func initTimeout() {
 
 const (
 	WELCOME  = `
- MuMax 2.0.1623 alpha
+ MuMax 2.0.1603 alpha
  (C) Arne Vansteenkiste, Ben Van de Wiele
      (Dynamat/EELAB Ghent University, Belgium), and
      Rémy Lasalle-Balier, Graham Rowlands, Mykola Dvornik.
