b1a240be9f77bef251ea330a45ca7124cb80ac02
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-01-31 15:41:36 +0100

    ready to test space-dependent aex

--- src/libmumax2/exchange6.cu
+++ src/libmumax2/exchange6.cu
@@ -10,10 +10,10 @@ extern "C" {
 #endif
 
 /// 2D, plane per plane, i=plane index
-__global__ void exchange6Kern(float* h, float* m, float* mPart0, float* mPart2,
+__global__ void exchange6Kern(float* h, float* m, float* mSat_map, float* Aex_map, float Aex2_Mu0Msat_mul, float* mPart0, float* mPart2,
                                int N0, int N1Part, int N2,
                                int wrap0, int wrap2,
-                               float fac0, float fac1, float fac2, 
+                               float cellx_2, float celly_2, float cellz_2, 
                                int i){
 
   //  i is passed
@@ -22,6 +22,26 @@ __global__ void exchange6Kern(float* h, float* m, float* mPart0, float* mPart2,
   int I = i*N1Part*N2 + j*N2 + k; // linear array index
   
   if (j < N1Part && k < N2){
+
+
+	float mSat_mask;
+	if (mSat_map ==NULL){
+		mSat_mask = 1.0f;
+	}else{
+		mSat_mask = mSat_map[i];
+		if (mSat_mask == 0.0f){
+			mSat_mask = 1.0f; // do not divide by zero
+		}
+	}
+
+    float Aex2_Mu0Msat; // 2 * Aex / Mu0 * Msat
+    if (Aex_map==NULL){
+      Aex2_Mu0Msat = Aex2_Mu0Msat_mul / mSat_mask;
+	}else{
+      Aex2_Mu0Msat = (Aex2_Mu0Msat_mul / mSat_mask) * Aex_map[i];
+	}
+
+
 
 	float m0 = m[I]; // mag component of central cell 
 	float m1, m2 ;   // mag component of neighbors in 2 directions
@@ -50,7 +70,7 @@ __global__ void exchange6Kern(float* h, float* m, float* mPart0, float* mPart2,
     } 
 	m2 = m[idx]; 
 
-    float H = fac0 * ((m1-m0) + (m2-m0));
+    float H = Aex2_Mu0Msat * cellx_2 * ((m1-m0) + (m2-m0));
 
     // neighbors in Z direction
     if (k-1 >= 0){
@@ -75,7 +95,7 @@ __global__ void exchange6Kern(float* h, float* m, float* mPart0, float* mPart2,
     } 
 	m2 = m[idx];
    
-    H += fac2 * ((m1-m0) + (m2-m0));
+    H += Aex2_Mu0Msat * cellz_2 * ((m1-m0) + (m2-m0));
 
 	// Here be dragons.
     // neighbors in Y direction
@@ -102,7 +122,7 @@ __global__ void exchange6Kern(float* h, float* m, float* mPart0, float* mPart2,
 			m2 = m[I];
 		}
     } 
-    H += fac1 * ((m1-m0) + (m2-m0));
+    H += Aex2_Mu0Msat * celly_2 * ((m1-m0) + (m2-m0));
 
 	// Write back to global memory
     h[I] = H;
@@ -123,9 +143,9 @@ void exchange6Async(float** hx, float** hy, float** hz, float** mx, float** my, 
   dim3 blocksize(BLOCKSIZE, BLOCKSIZE, 1);
   //int NPart = N0 * N1Part * N2;
 
-  float fac0 = Aex2_mu0MsatMul/(cellSizeX * cellSizeX);
-  float fac1 = Aex2_mu0MsatMul/(cellSizeY * cellSizeY);
-  float fac2 = Aex2_mu0MsatMul/(cellSizeZ * cellSizeZ);
+  float cellx_2 = 1/(cellSizeX * cellSizeX);
+  float celly_2 = 1/(cellSizeY * cellSizeY);
+  float cellz_2 = 1/(cellSizeZ * cellSizeZ);
   //printf("exchange factors %g %g %g\n", fac0, fac1, fac2); // OK
 
 	int nDev = nDevice();
@@ -155,7 +175,7 @@ void exchange6Async(float** hx, float** hy, float** hz, float** mx, float** my, 
 			//printf("exch dev=%d mPart0=%p mPart2=%p\n", dev, mPart0, mPart2); // OK
 
   			for(int i=0; i<N0; i++){   // for all layers. TODO: 2D version
-    			exchange6Kern<<<gridsize, blocksize, 0, cudaStream_t(streams[dev])>>>(H[dev], M[dev], mPart0, mPart2, N0, N1Part, N2, periodic0, periodic2, fac0, fac1, fac2, i);
+    			exchange6Kern<<<gridsize, blocksize, 0, cudaStream_t(streams[dev])>>>(H[dev], M[dev], msat[dev], aex[dev], Aex2_mu0MsatMul, mPart0, mPart2, N0, N1Part, N2, periodic0, periodic2, cellx_2, celly_2, cellz_2, i);
 			}
   		}
 	}
