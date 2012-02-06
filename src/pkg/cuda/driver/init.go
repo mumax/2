d6797a3b5be741bfb0544d8d9dab082f883fda14
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-02-06 11:35:47 +0100

    cu.Init()

--- src/pkg/cuda/driver/init.go
+++ src/pkg/cuda/driver/init.go
@@ -11,9 +11,11 @@ import "C"
 
 import ()
 
-// Initialize the CUDA driver API
-func Init() {
-	err := Result(C.cuInit(C.uint(0))) // Flags must be 0
+// Initialize the CUDA driver API.
+// Currently, flags must be 0.  
+// If Init() has not been called, any function from the driver API will panic with ERROR_NOT_INITIALIZED.
+func Init(flags int) {
+	err := Result(C.cuInit(C.uint(flags)))
 	if err != SUCCESS {
 		panic(err)
 	}
