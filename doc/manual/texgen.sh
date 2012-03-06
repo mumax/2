f644b001157112084666ee2a03ebed921dd82a1c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-06 16:06:49 +0100

    fix manual

--- doc/manual/texgen.sh
+++ doc/manual/texgen.sh
@@ -11,4 +11,4 @@ cd $INITIALPATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MUMAX2BIN/../src/libmumax2
 export PYTHONPATH=$PYTHONPATH:$MUMAX2BIN/../src/python
 
-exec $MUMAX2BIN/../src/mumax2/texgen $ARGV
+exec $MUMAX2BIN/texgen $ARGV
