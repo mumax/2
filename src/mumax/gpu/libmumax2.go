7a0661ad158ca1d22d686ed6258ecc77fe100207
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-07 14:33:23 +0100

    rm libmumax2:setIndex*()

--- src/mumax/gpu/libmumax2.go
+++ src/mumax/gpu/libmumax2.go
@@ -778,29 +778,29 @@ func DiffRhoAsync(drho, j *Array, cellsize []float64, periodic []int, stream Str
 		(*C.CUstream)(unsafe.Pointer(&(stream[0]))))
 }
 
-// DEBUG: sets all values to their X (i) index
-func SetIndexX(dst *Array) {
-	C.setIndexX(
-		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
-		C.int(dst.size3D[0]),
-		C.int(dst.partSize[1]),
-		C.int(dst.size3D[2]))
-}
-
-// DEBUG: sets all values to their Y (j) index
-func SetIndexY(dst *Array) {
-	C.setIndexY(
-		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
-		C.int(dst.size3D[0]),
-		C.int(dst.partSize[1]),
-		C.int(dst.size3D[2]))
-}
-
-// DEBUG: sets all values to their Z (k) index
-func SetIndexZ(dst *Array) {
-	C.setIndexZ(
-		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
-		C.int(dst.size3D[0]),
-		C.int(dst.partSize[1]),
-		C.int(dst.size3D[2]))
-}
+//// DEBUG: sets all values to their X (i) index
+//func SetIndexX(dst *Array) {
+//	C.setIndexX(
+//		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
+//		C.int(dst.size3D[0]),
+//		C.int(dst.partSize[1]),
+//		C.int(dst.size3D[2]))
+//}
+//
+//// DEBUG: sets all values to their Y (j) index
+//func SetIndexY(dst *Array) {
+//	C.setIndexY(
+//		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
+//		C.int(dst.size3D[0]),
+//		C.int(dst.partSize[1]),
+//		C.int(dst.size3D[2]))
+//}
+//
+//// DEBUG: sets all values to their Z (k) index
+//func SetIndexZ(dst *Array) {
+//	C.setIndexZ(
+//		(**C.float)(unsafe.Pointer(&(dst.pointer[0]))),
+//		C.int(dst.size3D[0]),
+//		C.int(dst.partSize[1]),
+//		C.int(dst.size3D[2]))
+//}
