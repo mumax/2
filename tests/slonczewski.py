3c7f5dcaff3faa64a3580497a54928ae7e2c2b62
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-07 15:01:07 +0100

    tests/slonczewski.py gives output

--- tests/slonczewski.py
+++ tests/slonczewski.py
@@ -10,17 +10,31 @@ setcellsize(5e-9, 5e-9, 5e-9)
 load('micromagnetism')
 load('solver/rk12')
 load('slonczewski')
+
 savegraph("graph.png")
+
+setv('aj',1.0)
+setv('bj',0.3)
+setv('Pol',0.56)
 
 setv('Msat', 800e3)
 setmask('Msat', ellipse())
 setv('Aex', 1.3e-11)
-setv('alpha', 0.01)
-setv('dt', 0.5e-12)
-setv('m_maxerror', 1./100)
-setv('aj',1.0)
-setv('bj',0.3)
-setv('Pol',0.56)
+setv('alpha', 0.5)
+
+setv('dt', 1e-15)
+setv('maxdt', 1e-12)
+setv('m_maxerror', 1./500)
+
+m=[ [[[1]]], [[[1]]], [[[0]]] ]
+setarray('m', m)
+
+autosave("m", "png", [], 5e-12)
+autotabulate(["t", "<m>"], "m.txt", 1e-12)
+
+run(1e-9)
+
+
 
 j=makearray(3, 1, 1, 1)
 j[0][0][0][0] = 0
@@ -28,15 +42,8 @@ j[1][0][0][0] = 0
 j[2][0][0][0] = 1e8 # Z component
 setarray('j', j)
 
-m=[ [[[1]]], [[[1]]], [[[0]]] ]
-
-
 p=[ [[[-1]]], [[[0]]], [[[0]]] ]
-setarray('m', m)
 setarray('p', p)
-
-autosave("m", "png", [], 25e-12)
-autotabulate(["t", "<m>"], "m.txt", 10e-12)
 
 run(2.0e-9)
 
