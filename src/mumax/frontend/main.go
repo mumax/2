879ab632672001f8ea3cce5bfbebfda357cfa66a
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-13 11:47:04 +0100

    use -gpu=0 by default instead of -gpu=all

--- src/mumax/frontend/main.go
+++ src/mumax/frontend/main.go
@@ -36,7 +36,7 @@ var (
 	flag_version   *bool   = flag.Bool("v", false, "Print version info and exit")
 	flag_test      *bool   = flag.Bool("test", false, "Test CUDA and exit")
 	//flag_timeout   *string = flag.String("timeout", "", "Set a maximum run time. Units s,h,d are recognized.")
-	flag_gpus  *string = flag.String("gpu", "all", "Which GPUs to use. gpu=0, gpu=0:3, gpu=1,2,3, gpu=all")
+	flag_gpus  *string = flag.String("gpu", "0", "Which GPUs to use. gpu=0, gpu=0:3, gpu=1,2,3, gpu=all")
 	flag_sched *string = flag.String("sched", "auto", "CUDA scheduling: auto|spin|yield|sync")
 	flag_fft   *string = flag.String("fft", "5", "Override the FFT implementation (advanced)")
 )
