b9afac1a6b6ace787b30faba138acfa2912bf597
Mykola Dvornik <mykola.dvornik@gmail.com>
2012-04-11 13:37:22 +0200

    Proper setup for 3D grid. Zhang-Li still fails.

--- src/libmumax2/zhang-li_torque.cu
+++ src/libmumax2/zhang-li_torque.cu
@@ -2,6 +2,7 @@
 #include "multigpu.h"
 #include "gpu_conf.h"
 #include "gpu_safe.h"
+#include "stdio.h"
 #include <cuda.h>
 
 #ifdef __cplusplus
@@ -48,25 +49,25 @@ extern "C" {
 			return;
 		}  
 	  	
-		int j = blockIdx.x * blockDim.x + threadIdx.x;
-		int k = blockIdx.y * blockDim.y + threadIdx.y;
+		int k = blockIdx.x * blockDim.x + threadIdx.x;
+		int j = blockIdx.y * blockDim.y + threadIdx.y;
 		int i = blockIdx.z * blockDim.z + threadIdx.z;	
-	  
+		
       // First-order derivative 5-points stencil
 	   
 	  int xb2 = (i-2 >= 0)? i-2 : i;
 	  int xb1 = (i-1 >= 0)? i-1 : i;
-	  int xf1 = (i+2 < sx)? i+1 : i;
+	  int xf1 = (i+1 < sx)? i+1 : i;
 	  int xf2 = (i+2 < sx)? i+2 : i;
 	  
 	  int yb2 = (j-2 >= 0)? j-2 : j;
 	  int yb1 = (j-1 >= 0)? j-1 : j;
-	  int yf1 = (j+2 < sy)? j+1 : j;
+	  int yf1 = (j+1 < sy)? j+1 : j;
 	  int yf2 = (j+2 < sy)? j+2 : j;
 	  
 	  int zb2 = (k-2 >= 0)? k-2 : k;
 	  int zb1 = (k-1 >= 0)? k-1 : k;
-	  int zf1 = (k+2 < sz)? k+1 : k;
+	  int zf1 = (k+1 < sz)? k+1 : k;
 	  int zf2 = (k+2 < sz)? k+2 : k;
 	  
 	  float comm = j * sz + k;
@@ -89,7 +90,7 @@ extern "C" {
 						 zf1 + comm, 
 						 zf2 + comm);
 
-	  //int x0 = comm + k;
+	  int x0 = comm + k;
 	  	
 	  // Let's use 5-point stencil to avoid problems at the boundaries
 	  
@@ -109,13 +110,13 @@ extern "C" {
 		
 	  // Don't see a point of such overkill, nevertheless:
 	  float j_x = 0.0;
-      if(jx != NULL){ j_x += jx[I]; }
+      if(jx != NULL){ j_x += jx[x0]; }
 	  
 	  float j_y = 0.0;
-      if(jy != NULL){ j_y += jy[I]; }
+      if(jy != NULL){ j_y += jy[x0]; }
 	  
 	  float j_z = 0.0;
-      if(jz != NULL){ j_z += jz[I]; }
+      if(jz != NULL){ j_z += jz[x0]; }
 	  //-------------------------------------------------//
 	  		
 	  float3 j0 = make_float3(j_x, j_y, j_z);
@@ -136,9 +137,9 @@ extern "C" {
 	  float at = jj * pret;
 	  float ad = jj * pred;
 	  
-      sttx[I] = ((ad * mxdmxm.x) + (at * dmxm.x));
-      stty[I] = ((ad * mxdmxm.y) + (at * dmxm.y));
-      sttz[I] = ((ad * mxdmxm.z) + (at * dmxm.z));
+      sttx[x0] = ((ad * mxdmxm.x) + (at * dmxm.x));
+      stty[x0] = ((ad * mxdmxm.y) + (at * dmxm.y));
+      sttz[x0] = ((ad * mxdmxm.z) + (at * dmxm.z));
 	       
     } 
   }
@@ -156,12 +157,20 @@ __export__  void zhangli_async(float** sttx, float** stty, float** sttz,
   {
 
     // 1D configuration
-    dim3 gridSize, blockSize;
-    make1dconf(NPart, &gridSize, &blockSize);
+    //dim3 gridSize, blockSize;
+    //make1dconf(NPart, &gridSize, &blockSize);
+	// 3D instead
+	
+	dim3 gridSize(divUp(sz, BLOCKSIZE), divUp(sy, BLOCKSIZE), divUp(sx, BLOCKSIZE));
+    dim3 blockSize(BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
+	printf("[ZLSTT DEBUG] The grid size: %d x %d x %d\n", sz, sy, sx);
 	
 	float i12csx_2 = 1.0f / (12.0f * csx * csz);
 	float i12csy_2 = 1.0f / (12.0f * csy * csy);
 	float i12csz_2 = 1.0f / (12.0f * csz * csz);
+	
+	printf("[ZLSTT DEBUG] Step Prefactors are: %f & %f & %f\n", i12csx_2, i12csy_2, i12csz_2);
+	printf("[ZLSTT DEBUG] Prefactors are: %f & %f\n", pred, pret);
 	
 	int syz = sy * sz;
 	
