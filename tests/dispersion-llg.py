80d9f066ccf06a9240491884e3a6235f10e041d5
Mykola Dvornik <mykola@mdvornik-xps.(none)>
2012-04-28 01:29:04 +0200

    dispersion simulations script is fixed

--- tests/dispersion-llg.py
+++ tests/dispersion-llg.py
@@ -2,6 +2,7 @@ from mumax2 import *
 from mumax2_geom import *
 from mumax2_magstate import *
 from math import * 
+
 Nx = 1024
 Ny = 1
 Nz = 1
@@ -29,10 +30,30 @@ m=[ [[[1]]], [[[0]]], [[[0]]] ]
 setarray('m', m)
 
 # static applied field
-Bx = 1.0 / mu0 # 10kOe = 1 T
-By = 0.0 / mu0 # T
-Bz = 0.0 / mu0 # T  
-setv('B_ext', [Bx, By, Bz])
+Bx = 1.0 
+By = 0.0 
+Bz = 0.0 
+
+b=makearray(3, Nx, Ny, Nz)
+
+for kk in range(Nz):
+    for jj in range(Ny):
+        for ii in range(Nx):
+            b[0][ii][jj][kk] = 1
+            b[1][ii][jj][kk] = 0
+            b[2][ii][jj][kk] = 0
+            
+b[0][0][0][0] = 0
+b[1][0][0][0] = 1
+b[2][0][0][0] = 0
+b[0][1][0][0] = 1
+b[1][1][0][0] = 0
+b[2][1][0][0] = 0
+b[0][2][0][0] = 1
+b[1][2][0][0] = 0
+
+setmask('B_ext',b)
+# setv('B_ext', [Bx, By, Bz])
 
 # Get the ground state
 
@@ -48,29 +69,14 @@ setv('alpha', 0.01)
 setv('t', 0)
 setv('dt', 0.001e-12)
 
-
-add_to('H_eff','Brf')
-
-rfmask = makearray(3, Nx, Ny, Nz)
-rfmask[0][0][0][0] = 0
-rfmask[1][0][0][0] = 1
-rfmask[2][0][0][0] = 0
-rfmask[0][1][0][0] = 0
-rfmask[1][1][0][0] = 1
-rfmask[2][1][0][0] = 0
-rfmask[0][2][0][0] = 0
-rfmask[1][2][0][0] = 1
-rfmask[2][2][0][0] = 0
-setmask('Brf',rfmask)
-
-save("Brf","png",[])
+save("B_ext","png",[])
 
 tt = 3e-12 # 1/2*tt = bandwidth ~166 GHz
 
 # Set up an rf applied field
 
 N = 2048 # 2048 timesteps, ~6 ns
-fcut = 100e9 # sinc cutoff, Hz
+fcut = 100e9 # sinc cutoff, H           z
 Brf0 = 0.1 # 1000 Oe = 0.1 T
 
 
@@ -78,16 +84,17 @@ for i in range(N):
         t = tt * i
         arg = 2 * pi * fcut * t
         if arg != 0.0:
-            By = Brf0 * sin(arg) / arg
+            Brfy = Brf0 * sin(arg) / arg
         else:
-            By = Brf0
-        setpointwise('Brf', t, [0, By / mu0, 0])
+            Brfy = Brf0
+        setpointwise('B_ext', t, [Bx, Brfy, 0])
 	
 TT = N * tt
 
 autosave("m", "png", [], tt)
 autosave("m", "ovf", [], tt)
 autotabulate(["t", "<m>"], "m.txt", tt)
+autotabulate(["t", "<Brf>"], "Brf.txt", tt)
 
 run(6e-9)
 
