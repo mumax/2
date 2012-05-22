99582b78441f446c2bd6612706fb584d23560f8f
Mykola Dvornik <mykola@mdvornik-xps.(none)>
2012-05-23 01:00:46 +0200

    tried binary blob from nvidia.com instead of the one from repo's.

--- src/cuda/curand/generator.go
+++ src/cuda/curand/generator.go
@@ -6,7 +6,7 @@
 
 package curand
 
-//#cgo LDFLAGS:-L/usr/local/cuda/lib -L/usr/local/cuda/lib64 -L/usr/lib/nvidia/ -L/usr/lib64/nvidia/ -L/usr/lib/nvidia-current/ -LC:/opt/cuda/v4.2/lib/x64 -lcuda -lcudart -lcurand
+//#cgo LDFLAGS:-L/usr/local/cuda/lib -L/usr/local/cuda/lib64 -L/usr/lib/nvidia/ -L/usr/lib64/nvidia/ -L/usr/lib64/nvidia/lib64 -L/usr/lib/nvidia-current/ -LC:/opt/cuda/v4.2/lib/x64 -lcuda -lcudart -lcurand
 //#cgo CFLAGS:-I/usr/local/cuda/include/ -IC:/opt/cuda/v4.2/include -Wno-error
 //#include <curand.h>
 import "C"
