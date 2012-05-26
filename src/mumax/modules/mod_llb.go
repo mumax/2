d1db7bccc93865fcbfc7654ce42a6c642d849bc2
Mykola Dvornik <mykola@mdvornik-xps.(none)>
2012-05-26 13:44:27 +0200

    I have totally forgot to add mod_llb to repo

---
+++ src/mumax/modules/mod_llb.go
@@ -0,0 +1,43 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package modules
+
+import (
+	. "mumax/common"
+	. "mumax/engine"
+	//"mumax/gpu"
+)
+
+// Register this module
+func init() {
+	RegisterModule("llb", "Landau-Lifshitz-Baryakhtar equation", LoadLLB)
+}
+
+// The torque quant contains the Landau-Lifshitz torque τ acting on the reduced magnetization m = M/Msat.
+//	d m / d t =  τ  
+// with unit
+//	[τ] = 1/s
+// Thus:
+//	τ = gamma[ (m x h) - α m  x (m x h) ]
+// with:
+//	h = H / Msat
+// To keep numbers from getting extremely large or small, 
+// the multiplier is set to gamma, so the array stores τ/gamma
+func LoadLLB(e *Engine) {
+
+	LoadHField(e)
+	LoadMagnetization(e)
+	
+    e.LoadModule("baryakhtar")
+    
+    e.AddPDE1("m", "bdt")
+	e.AddPDE1("msat", "bdl")
+
+}
+
+
