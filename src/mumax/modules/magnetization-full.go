226ce5510c1e665c8740dcb6685ab7af021ad987
Mykola Dvornik <mykola@mdvornik-xps.(none)>
2012-06-01 18:21:00 +0200

    Introduce full magnetization vector quantity: Mf

---
+++ src/mumax/modules/magnetization-full.go
@@ -0,0 +1,23 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package modules
+
+// File provides the magnetization quantity
+// Author: Arne Vansteenkiste
+
+import (
+	. "mumax/engine"
+)
+
+// Load the magnetization and MSat, if not yet present.
+func LoadFullMagnetization(e *Engine) {
+	if !e.HasQuant("Mf") {
+		Mf := e.AddNewQuant("Mf", VECTOR, FIELD, Unit("A/m"), "full magnetization vector")
+		Mf.SetUpdater(&decomposeMUpdater{Mf: Mf})
+	}
+}
