1555bdedeed7fc3b05106940e4edee5d6ce7680c
Mykola Dvornik <mykola.dvornik@gmail.com>
2012-10-09 14:42:14 +0200

    add 'add_to_weighted' API call to support custom multipliers

--- tests/addto.py
+++ tests/addto.py
@@ -2,47 +2,35 @@ from mumax2 import *
 
 # Tests the add_to() api
 
-Nx = 128
-Ny = 32
+Nx = 1024
+Ny = 1024
 Nz = 1
 setgridsize(Nx, Ny, Nz)
 setcellsize(500e-9/Nx, 125e-9/Ny, 3e-9/Nz)
 
-load('micromagnetism')
-load('solver/rk12')
+load('zeeman')
 
-setv('Msat', 800e3)
-setv('Aex', 1.3e-11)
-setv('alpha', 1)
-setv('dt', 1e-12)
-setv('m_maxerror', 1./1000)
+add_to('H_eff', 'H_ext')
 
-m=[ [[[1]]], [[[1]]], [[[0]]] ]
-setarray('m', m)
+H=[[[[0.0]]],[[[0.0]]],[[[0.0]]]]
+He=[[[[1.0]]],[[[0.5]]],[[[0.1]]]]
+setarray('H_eff', H)
+setmask('H_ext', He)
+setv('H_ext', [1.0, 1.0, 1.0])
+
+Hval = getv('<H_eff>')
+print Hval
+if Hval[0] != 1.0 or Hval[1] != 0.5 or Hval[2] != 0.1:
+    print "error in add_to"
+    
+add_to_weighted('H_eff', 'H_ext1', -2.0)
+setmask('H_ext1', He)
+setmask('H_ext', He)
+setv('H_ext1', [1.0, 1.0, 1.0]) 
+Hval = getv('<H_eff>')
+print Hval
+if Hval[0] != -1.0 or Hval[1] != -0.5 or Hval[2] != -0.1:
+    print "error in add_to_weighted"
 
 
-autosave("m", "omf", ["Text"], 200e-12)
-autotabulate(["t", "<m>", "m_error", "dt"], "m.txt", 10e-12)
-
-steps(2)
-run(1e-9)
-
-add_to("H_eff", "H_ext")
-add_to("H_ext", "H1")
-add_to("H_ext", "H2")
-add_to("H_ext", "H3")
-
-Hx = -24.6E-3 / mu0
-Hy =   4.3E-3 / mu0
-Hz =   0      / mu0 
-
-setv('H1', [Hx, 0, 0])
-setv('H2', [0, Hy, 0])
-setv('H3', [0, 0, Hz])
-
-setv('alpha', 0.02)
-setv('dt', 0.2e-12)
-
-steps(2)
-run(1e-9)
 
