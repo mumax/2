58994abc637cee5841bbbe10640a0a15cb94ee06
Mykola Dvornik <mykola.dvornik@gmail.com>
2012-10-09 17:13:37 +0200

    add lattice bath module

---
+++ src/mumax/modules/mod_LTM.go
@@ -0,0 +1,31 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package modules
+
+import (
+	. "mumax/common"
+	. "mumax/engine"
+)
+
+const LfluxName = "Ql"
+const LspatFluxName = "Ql_spat"
+const LtempName = "Temp"
+const LrateName = "dTempds"
+const LcapacName = "Cp_l"
+const LcondName = "k_l"
+
+// Register this module
+func init() {
+	RegisterModule("temperature/LTM", "Lattice temperature model", LoadLTM)
+}
+
+func LoadLTM(e *Engine) {
+    LoadTM(e, LtempName, LfluxName, LrateName, LcapacName)
+}
+
+
