bba260afe302153b914ecb33190d8bee24a6bab8
Mykola Dvornik <mykola.dvornik@gmail.com>
2013-02-20 12:34:20 +0100

    complete 3TM model (toy model)

---
+++ src/mumax/modules/mod_sl_coupling.go
@@ -0,0 +1,27 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package modules
+
+import (
+	. "mumax/common"
+	. "mumax/engine"
+)
+
+const SLfluxName = "Qsl"
+const SLTiName = "Ts"
+const SLTjName = "Temp"
+const SLcoupName = "Gsl"
+
+// Register this module
+func init() {
+	RegisterModule("temperature/S-L", "Spin-Lattice coupling", LoadSL)
+}
+
+func LoadSL(e *Engine) {
+	LoadQinter(e, SLfluxName, SLTiName, SLTjName, SLcoupName)
+}
--- tests/XTM-test.py
+++ tests/XTM-test.py
@@ -11,15 +11,21 @@ setgridsize(Nx, Ny, Nz)
 setcellsize(64e-9/Nx, 64e-9/Ny, 2e-9/Nz)
 
 load('temperature/ETM')
-load('temperature/ETM/Qspatial')
 load('temperature/LTM')
-load('temperature/LTM/Qspatial')
+load('temperature/STM')
 load('temperature/E-L')
+load('temperature/S-L')
+load('temperature/E-S')
 
-add_to("Qe", "Qe_spat")
-add_to("Ql", "Ql_spat")
+#~ Electron-Lattice
 add_to_weighted("Qe", "Qel", 1.0)
 add_to_weighted("Ql", "Qel", -1.0)
+#~ Spin-Lattice
+add_to_weighted("Qs", "Qsl", 1.0)
+add_to_weighted("Ql", "Qsl", -1.0)
+#~ Electron-Spin
+add_to_weighted("Qe", "Qes", 1.0)
+add_to_weighted("Qs", "Qes", -1.0)
 
 load('solver/rk12')
 setv('dt', 1e-15)
@@ -27,23 +33,25 @@ setv('maxdt', 1e-12)
 setv('mindt', 1e-18)
 
 setv('Te_maxerror', 1./1000.)
-setv('Te_maxerror', 1./1000.)
 setv('Temp_maxerror', 1./1000.)
-setv('Temp_maxerror', 1./1000.)
+setv('Ts_maxerror', 1./1000.)
 
 savegraph("graph.png")
 
-setv('k_e', 1.0e3)
-setv('Cp_e', 2.0e5)
+setv('Cp_e', 1.0e3)
 
-setv('k_l', 1.0e2)
 setv('Cp_l', 1.0e6)
 
-setv('Gel', 0.6e18)
+setv('Cp_s', 2.0e5)
 
-T = [[[[0.0]]]]
+setv('Gel', 1.0e18)
+setv('Gsl', 0.6e17)
+setv('Ges', 5.0e18)
+
+T = [[[[100.0]]]]
 setarray('Te',  T)
 setarray('Temp', T)
+setarray('Ts', T)
 
 
 add_to("Qe", "Q")
@@ -52,7 +60,7 @@ T0 = 50e-15 # Time delay for the excitation
 dT = 10e-15 # FWHM of the excitation gaussian envelope
 dTT = 0.5 / (dT * dT) # FWHW squared
 Qamp = 0.7e22
-N = 2100 # 
+N = 4100 # 
 time = N * tt
 fine = 10
 N_fine = fine * N
@@ -62,16 +70,11 @@ for i in range(N_fine):
         t = tt_fine * float(i)
         Q = Qamp * exp(-1.0 * dTT * (t-T0)**2)
         setpointwise('Q', t, Q)
-        
-
 
 autotabulate(["t", "<Te>"], "Te.dat", tt)
 autotabulate(["t", "<Temp>"], "Tl.dat", tt)
-autotabulate(["t", "<Qel>"], "Qel.dat", tt)
-
-autosave("Te", "png", [], tt)
-autosave("Temp", "png", [], tt)
-
+autotabulate(["t", "<Ts>"], "Ts.dat", tt)
+autotabulate(["t", "Q"], "Q.dat", tt)
 run(2e-12)
 
 printstats()
