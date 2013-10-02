5987d68f3837ecb8c44e7ec856bf6e01388e7a75
Mykola Dvornik <mykola.dvornik@gmail.com>
2013-10-02 12:22:20 +0200

    do not use explicit names of temperature quantities in thermal coupling modules

--- src/mumax/modules/mod_es_coupling.go
+++ src/mumax/modules/mod_es_coupling.go
@@ -2,7 +2,7 @@
 //  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
-//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
 package modules
@@ -13,8 +13,8 @@ import (
 )
 
 const ESfluxName = "Qes"
-const ESTiName = "Te"
-const ESTjName = "Ts"
+const ESTiName = EtempName
+const ESTjName = StempName
 const EScoupName = "Ges"
 
 // Register this module
