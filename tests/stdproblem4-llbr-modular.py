88c3d0f6e0f3a10ed36020ef3ecdadf745467b53
Mykola Dvornik <mykola.dvornik@gmail.com>
2013-10-28 12:07:09 +0100

    update stdproblem4 test for LLBar

--- tests/stdproblem4-llbr-modular.py
+++ tests/stdproblem4-llbr-modular.py
@@ -1,40 +1,34 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: Mykola Dvornik
 from mumax2 import *
 from math import *
 # Standard Problem 4
 
-# define geometry
-
-# number of cells
 Nx = 128
 Ny = 32
 Nz = 1
 setgridsize(Nx, Ny, Nz)
 
-# physical size in meters
 sizeX = 500e-9
 sizeY = 125e-9
 sizeZ = 3e-9
 setcellsize(sizeX/Nx, sizeY/Ny, sizeZ/Nz)
 
-# load modules
-
-# LLB 
 load('exchange6')
 load('demag')
 load('zeeman')
 
-load('llbr')
-load('llbr/torque')
-#~ load('llbr/longitudinal')
-load('llbr/transverse')
-#~ load('llbr/nonlocal')
+load('llbar')
+load('llbar/torque')
+load('llbar/damping/nonconservative/00/local')
 
-add_to('llbr_RHS', 'llbr_torque')
-add_to('llbr_RHS', 'llbr_transverse')
-#~ add_to('llbr_RHS', 'llbr_long')
-#~ add_to('llbr_RHS', 'llbr_nonlocal')
+add_to('llbar_RHS', 'llbar_torque')
+add_to('llbar_RHS', 'llbar_local00nc')
 
-loadargs('maxtorque', [],["torque:llbr_torque"],[])
+loadargs('mfa/longfield', ["T:Te"],[],[])
+
+loadargs('maxtorque', [],["torque:llbar_torque"],[])
 load('solver/rk12')
 setv('mf_maxerror', 1e-4)
 
@@ -43,14 +37,13 @@ setv('maxdt', 1e-12)
 
 # set parameters
 # Py
-# set magnetization
 
 m=[ [[[1]]], [[[1]]], [[[0]]] ]
 setarray('m', m)
 
-#~ T=[[[[273]]]]
-#~ setarray('Te',T)
-#~ setv('Tc', 820)
+T=[[[[273]]]]
+setarray('Te',T)
+setv('Tc', 820)
 
 msat = makearray(1,Nx,Ny,Nz)            
 for kk in range(Nz):
@@ -83,41 +76,33 @@ Ms0 = 800e3
 setv('msat0', Ms0)
 setv('msat0T0', Ms0)
 Aex = 1.3e-11
-lex = Aex / (mu0 * Ms0 * Ms0) 
-print("l_ex^2: "+str(lex)+"\n")
-lambda_e = 0.0 * lex
+
 alpha=1.0
+setv('λ∥', [alpha, alpha, alpha])
 
-#~ setv('lambda', [alpha, alpha, alpha])
-#~ setv('lambda_e', [lambda_e, lambda_e, lambda_e])
-setv('mu', [alpha, alpha, alpha])
-
-#~ setv('kappa', 1e-3)
+setv('ϰ', 1e-3)
 setv('Aex', 1.3e-11)
 gamma=2.211e5
 gammall = gamma / (1.0+alpha**2)
-setv('gamma_ll',gammall)
+setv('γ_ll',gammall)
 setv('dt', 1e-17) # initial time step, will adapt
 
 #relax
 
-#~ autotabulate(["t", "<m>"], "m.txt", 1e-12)
-autotabulate(["t", "maxtorque"], "t.dat", 1e-13)
+autotabulate(["t", "maxtorque"], "t.dat", 1e-15)
+# autotabulate(["t", "<mf>"], "mf-pre.txt", 1e-15)
 run_until_smaller('maxtorque', 1e-3 * gammall * Ms0)
 
 alpha=0.02
 gammall = gamma / (1.0+alpha**2)
-setv('gamma_ll',gammall)
-#~ setv('lambda', [alpha, alpha, alpha])
-setv('mu', [alpha, alpha, alpha])
+setv('γ_ll',gammall)
+setv('ϰ', 1e-4)
+setv('λ∥', [alpha, alpha, alpha])
 setv('t', 0)        # re-set time to 0 so output starts at 0
 save("m","vtk",[])
 save("m","ovf",[])
 
-# save magnetization snapshots in OMF text format every 20ps
-#~ autosave("m", "gplot", [], 1e-12)
-# save a table with time and the average magnetization every 10ps
-autotabulate(["t", "<m>"], "m.txt", 1e-12)
+autotabulate(["t", "<m>"], "m.txt", 1e-15)
 
 
 # run with field
