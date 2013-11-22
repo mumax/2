93505fc25de929b6845ca1181df28356d1beb21d
Mykola Dvornik <mykola.dvornik@gmail.com>
2013-11-22 17:04:38 +0100

    update heat-demag example to reflect modules changes

--- examples/heat-demag.py
+++ examples/heat-demag.py
@@ -22,20 +22,17 @@ csZ = sZ/Nz
 
 setgridsize(Nx, Ny, Nz)
 setcellsize(csX, csY, csZ)
-# setperiodic(8,8,0)
+setperiodic(4,4,0)
 
 # LLBar
 load('exchange6')
 load('demag')
-load('zeeman')
+# load('zeeman')
 
 load('llbar')
 load('llbar/torque')
 load('llbar/damping/nonconservative/00/local')
 load('llbar/damping/nonconservative/02/local')
-
-loadargs('energy-density-dissipation-rate',[], ["m:mf","R:llbar_local00nc", "msat:msat0T0"],["Qmag:Qnc00"])
-loadargs('energy-density-dissipation-rate',[], ["m:mf","R:llbar_local02nc", "msat:msat0T0"],["Qmag:Qnc02"])
 
 load('temperature/ETM')
 load('temperature/LTM')
@@ -43,15 +40,17 @@ load('temperature/E-L')
 
 load('temperature/sum')
 
-loadargs('mfa/longfield', ["T:Te"],[],[])
-loadargs('mfa/msat0', ["T:Te"],[],[])
-loadargs('mfa/ϰ', ["T:Te"],[],[])
+loadargs('mfa/longfield', ["T:Teff"],[],[])
+loadargs('mfa/msat0', ["T:Teff"],[],[])
+loadargs('mfa/ϰ', ["T:Teff"],[],[])
+loadargs('mfa/energy-flow', [], ["mf:mf","R:llbar_local00nc"],["q_s:qs00"])
+loadargs('mfa/energy-flow', [], ["mf:mf","R:llbar_local02nc"],["q_s:qs02"])
 
 add_to('llbar_RHS', 'llbar_torque')
 add_to('llbar_RHS', 'llbar_local00nc')
 
-add_to("Qe", "Qnc00")
-add_to("Qe", "Qnc02")
+add_to_weighted("Qe", "qs00", 1.0)
+add_to_weighted("Qe", "qs02", 1.0)
 
 add_to("Qe", "Qlaser")
 add_to_weighted("Qe", "Qel", 1.0)
@@ -59,24 +58,28 @@ add_to_weighted("Ql", "Qel", -1.0)
 
 load('solver/am12')
 setv('mf_maxabserror', 1e-4)
-setv('mf_maxrelerror', 1e-3)
+setv('mf_maxrelerror', 1e-4)
 
 setv('Te_maxabserror', 1e-4)
-setv('Te_maxrelerror', 1e-3)
+setv('Te_maxrelerror', 1e-4)
 setv('Tl_maxabserror', 1e-4)
-setv('Tl_maxrelerror', 1e-3)
+setv('Tl_maxrelerror', 1e-4)
 
 savegraph("graph.png")
 
-Ms0 = 480e3
+Ms0 = 548e3
 
-T = [ [[[200.0]]] ]
+T = [ [[[300.0]]] ]
 setarray('Te', T)
 setarray('Tl', T)
 
-setv('Tc', 631)
-setv('n', 9.14e28)
-setv('J', 1.0/2.0)
+ro = 9.13830273141122913505e28
+Tc = 633
+J = 0.5
+
+setv('Tc', Tc)
+setv('n', ro)
+setv('J', J)
 
 # Py
 
@@ -103,16 +106,17 @@ setv('γ_LL', 2.211e5)
 #~ setv('B_ext',[Bx,By,Bz])
 
 setv('dt', 1e-15)
-setv('ϰ', 1e-4)
+setv('ϰ', 1.0)
 
 # Heat bath parameters
-setv('Cp_e', 1070.0)
-setv('Cp_l', 4.14e6)
-setv('Gel', 1.6e18)
+setv('Cp_e', 654.74383987282318086567) # mean of gamma(T=300) and gamma(T=700)
+setv('Cp_l', 3620637.32928679817905918058) # mean of C(T=300) and C(T=Tc)
+setv('Gel', 3.5e17) # at 300 K
+
 setv('R', 0.5)
 
-lbd = 0.02
-mu = 0.0002
+lbd = 0.0045
+mu = lbd * 1e-2
 setv('λ∥', [lbd, lbd, lbd])
 setv('μ∥', [mu, mu, mu])
 
@@ -121,6 +125,7 @@ T0 = 500e-15 # Time delay for the excitation
 dT = 80e-15 # FWHM of the excitation gaussian envelope
 dTT = 0.5 / (dT * dT) # FWHW squared
 Qamp = 1e21
+
 N = 3100 #
 time = N * tt
 fine = 10
@@ -137,11 +142,14 @@ autotabulate(["t", "<msat>"], "msat.txt", tt)
 autotabulate(["t", "<Te>"], "Te.txt", tt)
 autotabulate(["t", "<Teff>"], "T.txt", tt)
 autotabulate(["t", "<msat0>"], "msat0.txt", tt)
+autotabulate(["t", "<ϰ>"], "kappa.txt", tt)
 autotabulate(["t", "<mf>"], "mf.txt", tt)
-autotabulate(["t", "<Qnc00>"], "Qnc00.txt", tt)
-autotabulate(["t", "<Qnc02>"], "Qnc02.txt", tt)
+autotabulate(["t", "<qs00>"], "qs00.txt", tt)
+autotabulate(["t", "<qs02>"], "qs02.txt", tt)
 autotabulate(["t", "<Qe>"], "Qe.txt", tt)
 autotabulate(["t", "<Qlaser>"], "Qlaser.txt", tt)
+autotabulate(["t", "<Qel>"], "Qel.txt", tt)
+
 
 setv('maxdt', 1e-12)
 setv('mindt', 1e-17)
