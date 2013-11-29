ff2472beb8e537a9daefbf4eaa7693bb1f069e29
Mykola Dvornik <mykola.dvornik@gmail.com>
2013-11-29 12:15:54 +0100

    add sum reducer

--- src/mumax/engine/update_reduce.go
+++ src/mumax/engine/update_reduce.go
@@ -2,7 +2,7 @@
 //  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
-//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
 package engine
@@ -54,6 +54,40 @@ func (this *AverageUpdater) Update() {
 			for c := 0; c < this.in.nComp; c++ {
 				sum := this.reduce.Sum(&(this.in.Array().Comp[c]))
 				this.out.SetComponent(c, float64(sum)*this.in.multiplier[c]/float64(GetEngine().NCell()))
+			}
+		}
+	} else {
+		// The mask is not space dependant, so just copy the multiplier from the 'in' into the 'out'
+		if this.in.nComp == 1 {
+			this.out.SetScalar(this.in.multiplier[0])
+		} else {
+			for c := 0; c < this.in.nComp; c++ {
+				this.out.SetComponent(c, this.in.multiplier[c])
+			}
+		}
+	}
+}
+
+// ________________________________________________________________________________ sum
+
+// Updates an average quantity
+type SumReduceUpdater ReduceUpdater
+
+// Returns an updater that writes the average of in to out
+func NewSumReduceUpdater(in, out *Quant) Updater {
+	return (*SumReduceUpdater)(NewReduceUpdater(in, out))
+}
+
+func (this *SumReduceUpdater) Update() {
+	var sum float32 = 666
+	if this.in.IsSpaceDependent() {
+		if this.in.nComp == 1 {
+			sum = this.reduce.Sum(this.in.Array())
+			this.out.SetScalar(float64(sum) * this.in.multiplier[0])
+		} else {
+			for c := 0; c < this.in.nComp; c++ {
+				sum := this.reduce.Sum(&(this.in.Array().Comp[c]))
+				this.out.SetComponent(c, float64(sum)*this.in.multiplier[c])
 			}
 		}
 	} else {
