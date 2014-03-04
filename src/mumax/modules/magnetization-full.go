4cf2f153dc370e749aaa07ea49ddb25aa63f68e7
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-03-04 15:34:15 +0100

    get rid of the mf decomposition, since m and msat are deprecated now

--- src/mumax/modules/magnetization-full.go
+++ src/mumax/modules/magnetization-full.go
@@ -2,7 +2,7 @@
 //  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
-//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
 package modules
@@ -18,15 +18,10 @@ import (
 
 func LoadFullMagnetization(e *Engine) {
 
-	LoadMagnetization(e)
-
 	if !e.HasQuant("mf") {
 		e.AddNewQuant("msat0", SCALAR, MASK, Unit("A/m"), "the initial distribution of the saturation magnetization")
 		e.AddNewQuant("msat0T0", SCALAR, MASK, Unit("A/m"), "the value of the saturation magnetization at Te = 0")
-		mf := e.AddNewQuant("mf", VECTOR, FIELD, Unit(""), "complete magnetization vector reduced by equilibrium value of saturation magnetization")
-		mf.SetUpdater(&decomposeMUpdater{mf: mf})
-		e.Depends("mf", "msat0", "msat0T0")
-		e.Depends("m", "mf")
-		e.Depends("msat", "mf")
+		e.AddNewQuant("mf", VECTOR, FIELD, Unit(""), "complete magnetization vector reduced by equilibrium value of saturation magnetization")
+		e.Depends("mf", "msat0T0")
 	}
 }
